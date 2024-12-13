4a0fa4e0c6b7b721dd07fc6edeb10a1c78d2488e
darktoom <darktoom789@gmail.com>
2024-12-14 01:42:24 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,180 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace assignment4 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1 Create a program that asks the user to input three points (x1, y1), (x2, y2), and (x3, y3), and determines whether these points lie on a single straight line.
+            //int[] p1 = new int[2];
+            //int[] p2 = new int[2];
+            //int[] p3 = new int[2];
+
+            //Console.WriteLine("pls enter point 1");
+            //for (int i = 0; i < p1.Length; i++)
+            //{
+            //    p1[i] = int.Parse(Console.ReadLine());
+
+
+            //}
+            //Console.WriteLine("pls enter point 2");
+            //for (int i = 0;i<p2.Length;i++)
+            //{
+            //    p2[i]= int.Parse(Console.ReadLine());
+
+            //}
+            //Console.WriteLine("pls enter point 3");
+            //for(int i = 0;i<p3.Length ; i++)
+            //{
+            //    p3[i]= int.Parse(Console.ReadLine());
+
+            //}
+            //if (p1[0] == p1[1] && p2[0] == p2[1] && p3[0] == p3[1])
+            //{
+
+            //    Console.WriteLine("this is stright line");
+            //}
+            //else
+            //    Console.WriteLine("this is not stright line");
+
+
+            #endregion
+
+
+            #region 2- Within a company, the efficiency of workers is evaluated based on the duration required to complete a specific task.
+
+            //Console.WriteLine("pls enter number of hours");
+            //int hours = Convert.ToInt32(Console.ReadLine());
+            //if (hours < 3)
+            //{
+            //    Console.WriteLine("highly efficient");
+            //}
+            //else if (hours >=3&&hours < 4) {
+            //    Console.WriteLine("increase their speed");
+
+            //}
+            //else if (hours >=4&& hours<5)
+            //    Console.WriteLine("enhance their speed. ");
+            //else if (hours >=5)
+            //    Console.WriteLine("leave the company");
+
+
+
+
+            #endregion
+
+
+            #region Write a program that prints an identity matrix using for loop, in other words takes a value n from the user and shows the identity table of size n * n.
+
+            //int n= Convert.ToInt32(Console.ReadLine());
+            //int[,] arr= new int[n,n];
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            //{
+
+            //    for (int j = 0; j < arr.GetLength(1); j++)
+            //    {
+            //        if (i==j) { 
+            //        arr[i,j] = 1;
+
+            //        }
+
+
+
+            //    }
+
+            //}
+            //for (int i = 0;i < arr.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr.GetLength(1); j++) {
+            //        Console.WriteLine(arr[i,j]);
+            //    }
+
+            //  }
+
+
+
+            #endregion
+
+
+            #region Write a program in C# Sharp to find the sum of all elements of the array.
+            //int[] arr= { 1, 2, 3 };
+            //int sum=0;
+            //for (int i = 0; i < arr.Length; i++) {
+            //    sum += arr[i];
+
+            //}
+            //Console.WriteLine(sum);
+
+
+            #endregion
+
+
+            #region Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
+            //            int[] arr = {1,2,3};
+            //            int[] arr2 = { 4, 5, 6 };
+            //            int[] arr3 = new int[6];
+            //           Array.ConstrainedCopy(arr,0,arr3,0,arr.Length);
+            //         Array.ConstrainedCopy(arr2,0,arr3,3,arr2.Length);
+
+
+            //foreach (int i in arr3)
+            //            {
+            //                Console.WriteLine(i);
+            //            }
+
+
+            #endregion
+
+
+            #region Write a program in C# Sharp to find maximum and minimum element in an array
+
+            //int[] arr = { 1, 2, 3, 4, 5 };
+            //int max = 0;
+            //int min = 9999;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > max)
+            //    {
+            //        max = arr[i];
+
+            //    }
+            //    if (arr[i] < min) { 
+
+            //    min = arr[i];
+            //    }
+
+
+
+
+
+
+            //}   Console.WriteLine(max);
+            //Console.WriteLine(min);
+            #endregion
+
+            #region Write a Program to Print One Dimensional Array in Reverse Order
+
+            //  int[] arr = { 1, 2, 3, 5 };
+            //  Array.Reverse(arr);
+            //Console.WriteLine(string.Join(" ", arr));
+
+
+
+            #endregion
+
+
+           
+
+
+
+      
+
+
+
+        }
+    }    
+}
+
+
